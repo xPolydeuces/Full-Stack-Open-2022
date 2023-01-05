a8d555e1a12a7b3c766ebda72b91926a854642e2
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-05 19:36:58 +0100

    2.5: seperate module

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,33 +1,4 @@
-const Course = ({course}) => (
-  <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-  </div>
-)
-
-const Header = ({course}) => (
-  <h2>{course.name}</h2>
-)
-
-const Part = ({part}) => (
-  <p>{part.name} {part.exercises}</p>
-)
-
-const Content = ({course}) => (
-  <div>
-    {course.parts.map(part =>
-      <Part key={part.id} part={part}/>
-    )}
-  </div>
-)
-
-const Total = ({course}) => (
-  <b>total of {course.parts.reduce(
-    (s,p) => s + p.exercises,
-    0,
-  )} exercises</b>
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [

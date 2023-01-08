ec650585b44def8f5160ef62624eef77c4eaea19
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-08 11:46:26 +0100

    2.12* Data for countries, step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Filter = ({newFilter, handleFilterChange}) => (
+  <div>
+    find countries <input 
+      value={newFilter}
+      onChange={handleFilterChange}
+    />
+  </div>
+)
+
+const Countries = ({countriesToShow, newFilter}) => {
+  if (newFilter === '') {return}
+  if (countriesToShow.length > 10) {
+    return('Too many matches, specify another filter')
+  }
+  if (countriesToShow.length === 1) {
+    const country = countriesToShow[0]
+    const languages = Object.values(country.languages)
+    return(
+      <div>
+        <h2>{country.name.common}</h2>
+        <div>capital {country.capital[0]}</div>
+        <div>area {country.area}</div>
+        <h3>languages:</h3>
+        {languages.map(language =>
+          <ul>
+            <li key={language}>{language}</li>
+          </ul>
+          )
+        }
+        <img src={country.flags.png}/>
+      </div>
+    )
+  }
+  return(
+  countriesToShow.map(country =>
+    <>
+      <div key={country.cca3}>{country.name.common}</div>
+    </>
+  )
+)}
+
+function App() {
+
+  const [countries, setCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const countriesToShow = (newFilter === '')
+    ? countries
+    : countries.filter(country => country.name.common.toLowerCase().includes(newFilter.toLowerCase()))
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+  return (
+    <div>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      <Countries countriesToShow={countriesToShow} newFilter={newFilter}/>
+    </div>
+  );
+}
+
+export default App;

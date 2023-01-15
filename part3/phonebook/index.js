58a31fd63c0aa18ec324caec74999d1a1fd92db5
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-15 19:49:11 +0100

    3.11 phonebook full stack

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 

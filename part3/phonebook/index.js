f1f2363d6b65b2b8988309e9e069ecb155c61ce1
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-15 19:13:44 +0100

    3.9 phonebook backend step9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     { 

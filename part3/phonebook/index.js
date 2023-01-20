2cec1fa4bf2ac8bec116964ee4dfe95f948e0c43
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-20 12:44:14 +0100

    3.22: Lint configuration

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -13,82 +13,83 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-        response.send(`<div>
+	Person.find({}).then(persons => {
+		response.send(`<div>
         <p>Phonebook has info for ${persons.length} persons</p>
         <p>${new Date()}</p>
         </div>`)
-    })
+	})
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-    })
-    .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id).then(response.status(204).end())
+	Person.findByIdAndDelete(request.params.id).then(response.status(204).end())
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+	const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
-    
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-    })
-    .catch(error => next(error))
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
+
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
+	const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        { name, number},
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+	Person.findByIdAndUpdate(
+		request.params.id,
+		{ name, number },
+		{ new: true, runValidators: true, context: 'query' }
+	)
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    }
-    if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message})
-    }
-  
-    next(error)
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+
+	next(error)
 }
 
 app.use(errorHandler)
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,41 +1,42 @@
 const mongoose = require('mongoose')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+	.then(() => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    validate: {
-      validator: function (v) {
-        return /^(\d{2,3}-\d+)$/.test(v)
-      },
-      message:props => `${props.value} is not a valid phone number!`
-    }
-  },
+	name: {
+		type: String,
+		minLength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: function (v) {
+				return /^(\d{2,3}-\d+)$/.test(v)
+			},
+			message:props => `${props.value} is not a valid phone number!`
+		}
+	},
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Person', personSchema)

5c3c4cff9382793feafef4f3cd09fc2d00327363
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-20 10:49:54 +0100

    3.18*: Phonebook database step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -63,7 +63,7 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })

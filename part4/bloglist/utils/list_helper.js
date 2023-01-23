16f480e8907991691e832013712ade092b0497a9
Polydeuces <bartosz.kot.00@gmail.com>
2023-01-23 09:23:04 +0100

    4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,13 @@
 const dummy = (blogs) => (1)
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }

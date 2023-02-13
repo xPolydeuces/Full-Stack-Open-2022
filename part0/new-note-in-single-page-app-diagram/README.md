69c2884cd2651c7bbe710b32a2bb911e17247e2f
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-02-13 09:53:29 +0100

    Reset repository

--- part0/new-note-in-single-page-app-diagram/README.md
+++
@@ -1,5 +0,0 @@
-```mermaid
-sequenceDiagram
-browser->>server: HTTP POST https://studies.cs.helsinki.fi/exampleapp/new_note_spa
-server-->>browser: Status Code 201
-```
--- part2/phonebook/src/components/Person.js
+++
@@ -1,5 +0,0 @@
-const Person = ({person, deletePerson}) => (
-    <div>{person.name} {person.number} <button onClick={() => (deletePerson(person))}>delete</button></div>
-)
-
-export default Person

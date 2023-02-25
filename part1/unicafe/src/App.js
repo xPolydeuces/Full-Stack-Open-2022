5b738ce76c35cac206acb893e1bfe2826a3bc72c
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-02-26 00:09:40 +0100

    add Header.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
 import { useState } from 'react'
+import Header from './components/Header'
+import Button from './components/Button'
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -7,7 +11,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text='give feedback'></Header>
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+      <Header text='statistics'></Header>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }

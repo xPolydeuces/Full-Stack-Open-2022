06298b8b878e74c3cdee909f5bb08972caa1e719
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-02 00:41:08 +0100

    1.12*: anecdotes step1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Button from './components/Button'
 
 const App = () => {
   const anecdotes = [
@@ -11,12 +12,17 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+
+  const randomize = () => (
+    Math.floor(Math.random() * anecdotes.length)
+  )
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => setSelected(randomize)} text='next anecdote'/>
     </div>
   )
 }

1fa0e10b13052da23aa0697836d17444a6993514
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-06 10:48:36 +0100

    2.2: Course information step7

--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -1,5 +1,5 @@
 const Total = ({ parts }) => (
-  <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+  <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises </strong></p>
 )
 
 export default Total

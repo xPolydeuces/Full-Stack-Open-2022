a39789a5d3ac17d9df7a0616e211ffe441a95bb0
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-08 15:15:45 +0100

    init phonebook

---
+++ part2/phonebook/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}

10ec785c76977f6687cf6019cecfea44d16c3b2e
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-10 23:23:22 +0100

    WIP filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import Persons from './components/Persons'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id:1 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -32,9 +37,14 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter persons={ newFilter } handler={ handleFilterChange }/>
       <form onSubmit={ addPerson }>
         <div>
           name: <input value={ newName } onChange={ handleNameChange } />
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,7 @@
+const Filter = ({ persons, handler }) => (
+  <>
+    <p>filter shown with <input value={ value } onChange={ handler } /> </p>
+  </>
+)
+
+export default Filter

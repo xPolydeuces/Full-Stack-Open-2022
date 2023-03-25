cc77f1d25dcf56dcaf41e8a52b806b8cf7f983f6
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-25 22:01:47 +0100

    simply filter

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,9 +1,9 @@
 import Country from './Country'
 
 const Countries = ({ countries, filter }) => {
-  const countriesToShow = filter === ''
-    ? countries
-    : countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+  if (filter === '') return
+  const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+  if (countriesToShow.length > 10) return (<>Too many matches, specify another filter</>)
   return (
     <>{countriesToShow.map(country => <Country key={country.cca3} name={country.name.common} />)}</>
   )

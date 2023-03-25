06d03376566515bcbed7d8ea229c7633ace67ee1
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-25 22:16:46 +0100

    2.18* Data for countries, step1

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -4,6 +4,23 @@ const Countries = ({ countries, filter }) => {
   if (filter === '') return
   const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
   if (countriesToShow.length > 10) return (<>Too many matches, specify another filter</>)
+  if (countriesToShow.length === 1) {
+    const country = countriesToShow[0]
+    const languages = Object.values(country.languages)
+    console.log(languages)
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>capital {country.capital}</p>
+        <p>area {country.area}</p>
+        <h3>languages:</h3>
+        <ul>
+          {languages.map(language => <li key={language}>{language}</li>)}
+        </ul>
+        <img src={country.flags.svg} alt='Flag of the country' width='20%'/>
+      </div>
+    )
+  }
   return (
     <>{countriesToShow.map(country => <Country key={country.cca3} name={country.name.common} />)}</>
   )

c25b60281279cab68acd8d17fa239abf7665b43a
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-28 13:00:18 +0200

    3.3: Phonebook backend step3

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -28,13 +28,24 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                 <p>${Date()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
-                 <p>${Date()}</p>`)
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001

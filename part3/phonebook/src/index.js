0690a6ea387a75e84d1345e54998353acb3ca7b7
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-28 12:57:38 +0200

    3.2: Phonebook backend step2

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                 <p>${Date()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

6c2c12d45ab55ffbb0a05231fcba19e1f7a47c6c
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-03-29 23:30:25 +0200

    3.8*: Phonebook backend step8

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { 

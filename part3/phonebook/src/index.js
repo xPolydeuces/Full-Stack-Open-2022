612e4e0dacb1983907ed827497ecddad902ee0b7
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-02 23:33:10 +0200

    3.9 phonebook backend step9

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 

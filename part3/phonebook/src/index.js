ff0f4fb23621f14721fe1c24a5152e74c5980d55
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-03 23:21:07 +0200

    3.11 phonebook full stack

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -7,6 +7,7 @@ morgan.token('body', function (req) { return JSON.stringify(req.body) })
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [

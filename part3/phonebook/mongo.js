9d3d3babdd8441c44cd4d569ebbe5f210d33ca57
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-04 23:35:10 +0200

    init mongo.js

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://admin:${password}@cluster0.yqkmufx.mongodb.net/personApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: 'Assan Assanovic',
+  number: '123456789',
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})

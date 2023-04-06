8aeb2f7fa0e0e404f58892a055254ec24370ef2b
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-06 12:45:30 +0200

    3.14: Phonebook database, step2

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -17,8 +17,12 @@ app.get('/', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
-                 <p>${Date()}</p>`)
+  Person.find({}).then(persons => {
+    response.send(`<div>
+    <p>Phonebook has info for ${persons.length} persons</p>
+    <p>${new Date()}</p>
+    </div>`)
+})
 })
 
 app.get('/api/persons', (request, response) => {
@@ -28,50 +32,26 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id).then(response.status(204).end())
 })
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-      return response.status(400).json({
-          error: 'name or number missing'
-      })
-  }
-
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({
-        error: 'name must be unique'
-    })
-}
-
-  const person = {
-      id: Math.floor(Math.random() * 999),
-      name: body.name,
-      number: body.number,
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+  new Person({
+    name: request.body.name,
+    number: request.body.number
+  }).save()
+  .then(person => {
+    response.json(person)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

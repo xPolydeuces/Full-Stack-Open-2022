dec9e2b43ad14671e90b3fb0cddf7dcaefd56307
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-09 19:51:30 +0200

    add blogs.js controller

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,23 +9,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {

214ae4514bf6a4379b82d261a7f330a9da7d34a8
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-09 19:32:09 +0200

    add blog.js model

---
+++ part4/bloglist/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

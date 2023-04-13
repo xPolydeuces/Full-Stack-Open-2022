38558d592641d9b3ff99e4cd2fa0e3f4fe2aa2ec
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-13 22:09:12 +0200

    4.8: Blog list tests, step1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs is returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})

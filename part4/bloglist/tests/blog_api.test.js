4aa0c84594c095ee7ea99cb9059cfcec011fbd33
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-04-15 23:28:02 +0200

    refactor test code

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -7,71 +7,76 @@ const helper = require('./test_helper')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initally some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('unique identifier property is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('correct amount of blogs is returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'Test blog',
+      author: 'Test-Chan',
+      url: 'https://testingpost.com/',
+      likes: 21
+    }
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-test('unique identifier property is named id', async () => {
-  const response = await api.get('/api/blogs')
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  expect(response.body[0].id).toBeDefined()
-})
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain('Test blog')
+  })
 
-test('POST operation is successful', async () => {
-  const testBlog = {
-    title: 'Test blog',
-    author: 'Test-Chan',
-    url: 'https://testingpost.com/',
-    likes: 21
-  }
-  await api.post('/api/blogs')
-    .send(testBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  test('if the likes property is missing, it will default to 0', async () => {
+    const newBlog = {
+      title: 'Test blog',
+      author: 'Test-Chan',
+      url: 'https://testingpost.com/'
+    }
 
-  const blogsAtEnd = await helper.blogsInDb()
+    await api.post('/api/blogs').send(newBlog)
 
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-})
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
+  })
 
-test('if the likes property is missing, it will default to 0', async () => {
-  const testBlog = {
-    title: 'Test blog',
-    author: 'Test-Chan',
-    url: 'https://testingpost.com/'
-  }
+  test('if title or url properties are missing, fails status code 400', async () => {
+    const newBlog = {
+      author: 'Test-Chan',
+      likes: 21
+    }
 
-  await api.post('/api/blogs').send(testBlog)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
-})
-
-test('if the title or url properties are missing, responds with the status code 400 Bad Request', async () => {
-  const testBlog = {
-    author: 'Test-Chan',
-    likes: 21
-  }
-
-  await api.post('/api/blogs')
-    .send(testBlog)
-    .expect(400)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
 
